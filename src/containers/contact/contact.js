2b03f09d613d09aa136f5d2cadb491059bf56b4f
momojunzi <mark.holthaus@gmail.com>
2018-05-28 21:33:11 -0700

    added loadbalancer

--- src/containers/contact/contact.js
+++ src/containers/contact/contact.js
@@ -15,7 +15,7 @@ class Contact extends Component {
     event.preventDefault();
     const email = this.state.email;
     console.log(email);
-    axios.post('http://localhost:3001/api/email', email )
+    axios.post('http://35.232.48.52:3001/api/email', email )
       .then(result => console.log(result))
       .catch(err => console.log(err))
   }
